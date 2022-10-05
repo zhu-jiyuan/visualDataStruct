2269ce1bb4c8d0dd77dc59ab9f83c4916e337b39
zhu-jiyuan <848433033@qq.com>
2022-10-05 22:33:52 +0800

    add count sort.

---
+++ count_sort/main.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/10/5 22:07
+# @Author  : zhujiyuan
+
+from matplotlib import pyplot as plt
+import random
+
+def getTestSet():
+    x = list(range(100))
+    y = [random.randint(0,300) for i in range(100)]
+
+    return x,y
+
+#get test set
+xs,ys = getTestSet()
+print(ys)
+def plotBar():
+    plt.clf()
+    plt.xlim(0,105)
+
+    plt.gca().xaxis.set_major_locator(plt.MultipleLocator(5))
+
+    plt.bar(xs,ys)
+    plt.pause(3)
+
+ts = ys.copy()
+cnt = 0
+
+
+def count_sort():
+    #init 10 list
+    #keyword is 3
+
+    buckets = [[] for i in range(10)]
+
+    for i in range(3):
+        #sort with keyword to bucket
+        for num in ys:
+            buckets[int(num/(10**i))%10].append(num)
+
+
+        #recycle
+        index = 0
+        for bucket in  buckets:
+            for v in bucket:
+                #print(f"index={index},v={v}")
+                ys[index]=v
+                #
+                index+=1
+        buckets = [[] for i in range(10)]
+        plotBar()
+count_sort()
+
+
+plt.show()
+print(ys)
+ts.sort()
+
+for i in zip(ys,ts):
+    if i[0]!=i[1]:
+        print("false!!")
+        break
